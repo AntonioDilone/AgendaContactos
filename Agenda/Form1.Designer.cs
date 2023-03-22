6889ea557d217876dca87f4683f45fd2ae6e710e
Antonio Dilone <antoniodilone26@gmail.com>
2023-03-22 14:01:50 -0400

    Agregar archivos de proyecto.

---
+++ Agenda/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace Agenda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TxtTelefono = new System.Windows.Forms.MaskedTextBox();
+            this.TxtCedula = new System.Windows.Forms.MaskedTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.TxtNombre = new System.Windows.Forms.TextBox();
+            this.TxtGrupo = new System.Windows.Forms.ComboBox();
+            this.TxtCorreo = new System.Windows.Forms.TextBox();
+            this.TxtDireccion = new System.Windows.Forms.TextBox();
+
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(50, 302);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 68);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Añadir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(248, 302);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 68);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Eliminar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.BtnEli_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Telefono";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 188);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Cedula";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(378, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Correo";
+            this.label4.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(378, 133);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 16);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Direccion";
+            // 
+            // TxtTelefono
+            // 
+            this.TxtTelefono.Location = new System.Drawing.Point(125, 133);
+            this.TxtTelefono.Mask = "000-000-0000";
+            this.TxtTelefono.Name = "TxtTelefono";
+            this.TxtTelefono.Size = new System.Drawing.Size(214, 22);
+            this.TxtTelefono.TabIndex = 7;
+            // 
+            // TxtCedula
+            // 
+            this.TxtCedula.Location = new System.Drawing.Point(125, 188);
+            this.TxtCedula.Mask = "000-0000000-0";
+            this.TxtCedula.Name = "TxtCedula";
+            this.TxtCedula.Size = new System.Drawing.Size(214, 22);
+            this.TxtCedula.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(445, 302);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 68);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Actualizar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.BtnAct_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(378, 194);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 16);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Grupo";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(646, 302);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(124, 68);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Borrar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // TxtNombre
+            // 
+            this.TxtNombre.Location = new System.Drawing.Point(125, 75);
+            this.TxtNombre.Multiline = true;
+            this.TxtNombre.Name = "TxtNombre";
+            this.TxtNombre.Size = new System.Drawing.Size(214, 22);
+            this.TxtNombre.TabIndex = 12;
+            // 
+            // TxtGrupo
+            // 
+            this.TxtGrupo.FormattingEnabled = true;
+            this.TxtGrupo.Items.AddRange(new object[] {
+            "Familia",
+            "Amigos",
+            "Trabajo",
+            "Otros"});
+            this.TxtGrupo.Location = new System.Drawing.Point(445, 188);
+            this.TxtGrupo.Name = "TxtGrupo";
+            this.TxtGrupo.Size = new System.Drawing.Size(220, 24);
+            this.TxtGrupo.TabIndex = 13;
+            // 
+            // TxtCorreo
+            // 
+            this.TxtCorreo.Location = new System.Drawing.Point(445, 72);
+            this.TxtCorreo.Name = "TxtCorreo";
+            this.TxtCorreo.Size = new System.Drawing.Size(220, 22);
+            this.TxtCorreo.TabIndex = 14;
+            // 
+            // TxtDireccion
+            // 
+            this.TxtDireccion.Location = new System.Drawing.Point(445, 133);
+            this.TxtDireccion.Name = "TxtDireccion";
+            this.TxtDireccion.Size = new System.Drawing.Size(220, 22);
+            this.TxtDireccion.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(226)))), ((int)(((byte)(210)))));
+            this.ClientSize = new System.Drawing.Size(839, 533);
+            this.Controls.Add(this.TxtDireccion);
+            this.Controls.Add(this.TxtCorreo);
+            this.Controls.Add(this.TxtGrupo);
+            this.Controls.Add(this.TxtNombre);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.TxtCedula);
+            this.Controls.Add(this.TxtTelefono);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dgvContacto;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.MaskedTextBox TxtTelefono;
+        private System.Windows.Forms.MaskedTextBox TxtCedula;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox TxtNombre;
+        private System.Windows.Forms.ComboBox TxtGrupo;
+        private System.Windows.Forms.TextBox TxtCorreo;
+        private System.Windows.Forms.TextBox TxtDireccion;
+    }
+}
+
---
+++ Agenda/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Data.SqlTypes;
+using System.Collections;
+
+namespace Agenda
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            insertar();
+        }
+
+        private void BtnEli_Click(object sender, EventArgs e)
+        {
+            eliminar();
+        }
+
+        private void BtnAct_Click(object sender, EventArgs e)
+        {
+            consultarTodo();
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e) 
+        {
+            
+        }
+
+        private void consultarTodo()
+        {
+            SqlConnection conn = new SqlConnection();
+
+            conn.ConnectionString = "Data Source=LAPTOP-QM5FFGMO\\SQLEXPRESS;Initial Catalog=Agenda;Integrated Security=True";
+
+            conn.Open();
+
+            string str = "SELECT CEDULA, NOMBRE, TELEFONO,CORREO,GRUPO, DIRECCION FROM CONTACTO";
+
+            SqlCommand cmd = new SqlCommand(str);
+
+            cmd.Connection = conn;
+
+            //Inicio para llenar DataGridView
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+            DataTable dt = new DataTable();
+
+            da.Fill(dt);
+
+            dgvContacto.DataSource = dt;
+            //Final para llenar DataGridView
+
+
+
+            //inicio interar uno a uno
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+
+                string datos = "Cedula: " + dr.GetInt32(0) + " nombre: " + dr.GetString(1);
+
+                MessageBox.Show(datos);
+
+            }
+            //Fin para interar uno a uno
+            conn.Close();
+        }
+        private void eliminar()
+        {
+
+            SqlConnection conn = new SqlConnection();
+
+            conn.ConnectionString = "Data Source=LAPTOP-QM5FFGMO\\SQLEXPRESS;Initial Catalog=Agenda;Integrated Security=True";
+
+            conn.Open();
+
+            string str = "DELETE CONTACTO WHERE CEDULA = @CEDULA";
+
+            SqlCommand cmd = new SqlCommand(str);
+
+            cmd.Parameters.AddWithValue("@CEDULA", TxtCedula.Text);
+
+            cmd.Connection = conn;
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+
+            consultarTodo();
+        }
+
+        private void insertar()
+        {
+            SqlConnection conn = new SqlConnection();
+
+            conn.ConnectionString = "Data Source=LAPTOP-QM5FFGMO\\SQLEXPRESS;Initial Catalog=Agenda;Integrated Security=True";
+
+            conn.Open();
+
+            string str = "INSERT INTO Contacto (CEDULA,NOMBRE,TELEFONO,CORREO,GRUPO, DIRECCION) VALUES(@CEDULA, @NOMBRE, @TELEFONO, @CORREO, @GRUPO, @DIRECCION)";
+
+            SqlCommand cmd = new SqlCommand(str);
+
+            cmd.Parameters.AddWithValue("@CEDULA", TxtCedula.Text);
+            cmd.Parameters.AddWithValue("@NOMBRE", TxtNombre.Text);
+            cmd.Parameters.AddWithValue("@TELEFONO", TxtTelefono.Text);
+            cmd.Parameters.AddWithValue("@CORREO", TxtCorreo.Text);
+            cmd.Parameters.AddWithValue("@GRUPO", TxtGrupo.Text);
+            cmd.Parameters.AddWithValue("@DIRECCION", TxtDireccion.Text);
+
+            cmd.Connection = conn;
+            cmd.Connection = conn;
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+
+            consultarTodo();
+        }
+
+    }
+
+
+}
